de2bfb4a6d52a6d3d64c41eba57381551498657b
Masayoshi Kokuryo <masakoku@gmail.com>
2026-01-25 08:53:42 +0900

    openvswitch-nfv for alma9

--- src/plur_linux/recipes/almalinux9/openvswitch.py
+++ src/plur_linux/recipes/almalinux9/openvswitch.py
@@ -2,4 +2,4 @@ from plur_linux.recipes.openvswitch import redhat
 
 
 def install(session):
-    redhat.dict_ovs['almalinux9']['caracal'](session)
+    redhat.dict_ovs['almalinux9'](session)
--- src/plur_linux/recipes/openvswitch/redhat.py
+++ src/plur_linux/recipes/openvswitch/redhat.py
@@ -1,4 +1,6 @@
+import re
 from mini import misc
+from mini.ansi_colors import yellow
 from plur import base_shell
 from plur import session_wrap
 
@@ -27,12 +29,26 @@ def install_from_centos_release_openstack(version):
     return func
 
 
+@session_wrap.sudo
+def install_from_centos_release_nfv(session):
+    base_shell.run(session, "dnf install -y centos-release-nfv-openvswitch")
+    capt = base_shell.run(session, 'dnf search openvswitch | egrep "^openvswitch[3-9].[0-9].x86_64" | cat')
+    install_target = ''
+    for line in re.split('\r?\n', capt):
+        if line.startswith('openvswitch'):
+            sp = line.split(' :')
+            if len(sp) == 2:
+                install_target = sp[0]
+    [base_shell.run(session, a) for a in misc.del_indent_lines(f"""
+    dnf install -y {install_target} NetworkManager-ovs
+    systemctl enable --now openvswitch
+    """)]
+
+
 dict_ovs = {
     'almalinux8': {
         # this doesn't work by EOL 2024/6
         'yoga': install_from_rdo_archived
     },
-    'almalinux9': {
-        'caracal': install_from_centos_release_openstack('caracal')
-    },
+    'almalinux9': install_from_centos_release_nfv,
 }
